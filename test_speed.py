0c9c4b8edb2ad4894000ec865a8a259d4ee07956
yeyu <869690408@qq.com>
2025-09-19 12:31:11 +0800

    Update test_speed.py

--- test_speed.py
+++ test_speed.py
@@ -1,1 +1,54 @@
 
+
+
+import torch
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+
+    import time
+    input = torch.rand((1, 3, 256,256)).cuda().float()
+    model = torch.jit.load('./mobilenet0.029371085.pt').cuda().float().eval()
+
+    iterations = None
+
+    with torch.no_grad():
+        for _ in range(10):
+            model(input)
+
+        if iterations is None:
+            elapsed_time = 0
+            iterations = 100
+            while elapsed_time < 1:
+                torch.cuda.synchronize()
+                torch.cuda.synchronize()
+                t_start = time.time()
+                for _ in range(iterations):
+                    model(input)
+                torch.cuda.synchronize()
+                torch.cuda.synchronize()
+                elapsed_time = time.time() - t_start
+                iterations *= 2
+            FPS = iterations / elapsed_time
+            iterations = int(FPS * 6)
+
+        print('=========Speed Testing=========')
+        torch.cuda.synchronize()
+        torch.cuda.synchronize()
+        t_start = time.time()
+        for _ in range(iterations):
+            model(input)
+        torch.cuda.synchronize()
+        torch.cuda.synchronize()
+        elapsed_time = time.time() - t_start
+        latency = elapsed_time / iterations * 1000
+    torch.cuda.empty_cache()
+    FPS = 1000 / latency
+    print(FPS)
